350f743efbf8beec85a249b03206df78acfd0746
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2014-03-23 23:17:54 +0800

    removeGo and replaceStart

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Chrome extension that enchances TipidPC.com's UX
 
 Version 0.1
+
+## Features
+
+* replace **Prev** and **Next** buttons with links you can open in a new tab/window
+* remove **Go** button (Forums) because it's just useless
+* replace **Start a topic** button with a link, and it will automatically set the **Forum section** for you on the New topic page
--- js/content-script.js
+++ js/content-script.js
@@ -2,8 +2,23 @@ var EXTENSION_ID = chrome.i18n.getMessage('@@extension_id');
 
 function pager() {
   page = $('.pager tbody tr td');
-  prev = page.find('input[value="Prev"]');
-  next = page.find('input[value="Next"]');
+
+  //prev & next
+  page.replaceNav();
+
+  //forum jump
+
+  //go
+  page.removeGo();
+
+  //start a topic
+  page.replaceStart();
+}
+
+jQuery.fn.replaceNav = function() {
+  var prev = $(this).find('input[value="Prev"]');
+  var next = $(this).find('input[value="Next"]');
+
   prev.each(function() {
     if(!$(this).is(':disabled')) {
       var link = /^document\.location\.href\=\'(.*)\'/.exec($(this).attr('onclick'))[1];
@@ -15,6 +30,7 @@ function pager() {
       $(this).remove();
     }
   });
+
   next.each(function() {
     if(!$(this).is(':disabled')) {
       var link = /^document\.location\.href\=\'(.*)\'/.exec($(this).attr('onclick'))[1];
@@ -26,9 +42,49 @@ function pager() {
       $(this).remove();
     }
   });
-  //.replaceWith()
-}
+};
+
+jQuery.fn.replaceStart = function() {
+  var start = $(this).find('input[value="Start a Topic"]');
+
+  if (start.length) {
+    var forum = get_param('sec');
+    start.each(function() {
+      var new_el = $('<a/>').prop('href', 'newtopic.php?sec='+forum)
+                            .text('Start a Topic');
+      $(this).replaceWith(new_el);
+    });
+  }
+};
+
+jQuery.fn.removeGo = function() {
+  var go = $(this).find('input[value="Go"]');
+
+  if (go.length) {
+    go.each(function() {
+      $(this).remove();
+    });
+  }
+};
 
 $(function() {
   pager();
 });
+
+
+//Utility
+
+//from http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+function get_param(sParam)
+{
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++)
+  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam)
+    {
+      return sParameterName[1];
+    }
+  }
+}
